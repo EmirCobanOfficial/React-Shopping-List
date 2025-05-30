aa73cce21ace5bc17f2324613b5305d7eced01f2
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-05-30 11:50:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,45 @@ import FilterButtons from "./components/FilterButtons";
 import ListItem from "./components/ListItem";
 import ClearButton from "./components/ClearButton";
 import Item from "./components/Item";
+import { useState } from "react";
 
 const urunler = [
   { id: 1, name: "Yumurta", completed: true },
   { id: 2, name: "Peynir", completed: true },
   { id: 3, name: "Zeytin", completed: false },
-  { id: 4, name: "Et", completed: true },
+  { id: 4, name: "Et", completed: false },
   { id: 5, name: "Tavuk", completed: true },
 ];
 
 export default function App() {
+  const [items, setItems] = useState(urunler);
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((i) => i.id != id));
+  }
+
+  function handleUpdateItem(id) {
+    const updatedItems = (items) =>
+      items.map((item) =>
+        item.id == id ? { ...item, completed: !item.completed } : item
+      );
+    setItems(updatedItems);
+  }
+
   return (
     <div className="container">
       <Header />
-      <AddItemForm />
+      <AddItemForm onAddItem={handleAddItem} />
       <FilterButtons />
-      <ListItem urunler={urunler} />
+      <ListItem
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onUpdateItem={handleUpdateItem}
+      />
       <ClearButton />
     </div>
   );
