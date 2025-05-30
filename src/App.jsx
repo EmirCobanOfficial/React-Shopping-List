012eed8074dcd3394983c9cd2579cd33ade56d85
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-05-30 10:43:13 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,26 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 import AddItemForm from "./components/AddItemForm";
 import Header from "./components/Header";
-import NoItem from "./components/NoItem";
 import FilterButtons from "./components/FilterButtons";
 import ListItem from "./components/ListItem";
 import ClearButton from "./components/ClearButton";
+import Item from "./components/Item";
+
+const urunler = [
+  { id: 1, name: "Yumurta", completed: true },
+  { id: 2, name: "Peynir", completed: true },
+  { id: 3, name: "Zeytin", completed: false },
+  { id: 4, name: "Et", completed: true },
+  { id: 5, name: "Tavuk", completed: true },
+];
 
 export default function App() {
   return (
     <div className="container">
       <Header />
-
       <AddItemForm />
-
-      <NoItem />
-
       <FilterButtons />
-
-      <ListItem />
-
+      <ListItem urunler={urunler} />
       <ClearButton />
     </div>
   );
