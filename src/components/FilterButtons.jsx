04719e1610196becdf44e6f4aff99eb65a2db441
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-06-04 16:03:34 +0300

    Update FilterButtons.jsx

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,14 +1,41 @@
-export default function FilterButtons() {
+export default function FilterButtons({
+  FilterButton,
+  setFilterButton,
+  onClearItems,
+}) {
   return (
-    <div className="filter-buttons border rounded p-3 mb-3">
-      <button item-filter="all" className="btn btn-primary me-1">
-        All
-      </button>
-      <button item-filter="incomplete" className="btn btn-secondary me-1">
-        Incomplete
-      </button>
-      <button item-filter="completed" className="btn btn-secondary">
-        Completed
+    <div className="border rounded p-3 mb-3 d-flex justify-content-between">
+      <div>
+        <button
+          item-filter="all"
+          className={`btn me-1 ${
+            FilterButton === "all" ? "btn-primary" : "btn-secondary"
+          }`}
+          onClick={() => setFilterButton("all")}
+        >
+          All
+        </button>
+        <button
+          item-filter="incomplete"
+          className={`btn me-1 ${
+            FilterButton === "incomplete" ? "btn-primary" : "btn-secondary"
+          }`}
+          onClick={() => setFilterButton("incomplete")}
+        >
+          Incomplete
+        </button>
+        <button
+          item-filter="completed"
+          className={`btn me-1 ${
+            FilterButton === "completed" ? "btn-primary" : "btn-secondary"
+          }`}
+          onClick={() => setFilterButton("completed")}
+        >
+          Completed
+        </button>
+      </div>
+      <button className="btn btn-danger clear" onClick={onClearItems}>
+        Clear
       </button>
     </div>
   );
